d1ad0d29776921b07453853bc325ecfd85a7d315
shahramtaheri <68184774+shahramtaheri@users.noreply.github.com>
2025-12-14 10:14:41 +0300

    Create seed.py

---
+++ src/utils/seed.py
@@ -0,0 +1,17 @@
+# src/utils/seed.py
+from __future__ import annotations
+
+import random
+import numpy as np
+import torch
+
+
+def set_seed(seed: int, deterministic: bool = True) -> None:
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
